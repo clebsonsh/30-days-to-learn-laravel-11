628c4137ecf7e80605af2427985d73015b5bbbd7
Clebson Moura <clebson.sh@gmail.com>
2024-06-09 21:53:19 -0300

    Forms and CSRF Explained (with Examples)

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -2,7 +2,7 @@
     <div class="space-y-4">
         {{ $jobs->links() }}
         @foreach ($jobs as $job)
-            <a href="{{ route('job', $job) }}" class="px-4 py-6 rounded-lg border border-gray-200 block">
+            <a href="{{ route('jobs.show', $job) }}" class="px-4 py-6 rounded-lg border border-gray-200 block">
                 <div class="text-blue-500 text-sm font-bold">
                     {{ $job->employer->name }}
                 </div>
--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,41 @@
 use App\Models\Job;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn () => view('home'))->name('home');
+Route::get('/', fn() => view('home'))->name('home');
 
-Route::get('/jobs', function () {
-    return view('jobs', [
-        'jobs' => Job::with('employer')->paginate(),
-    ]);
-})->name('jobs');
+Route::prefix('jobs')->name('jobs.')->group(function () {
+    Route::get('', function () {
+        $jobs = Job::query()
+            ->with('employer')
+            ->latest()
+            ->paginate();
 
-Route::get('/jobs/{job}', function (Job $job) {
-    return view('job', [
-        'job' => $job,
-    ]);
-})->name('job');
+        return view('jobs.index', [
+            'jobs' => $jobs,
+        ]);
+    })->name('index');
 
-Route::get('/contact', fn () => view('contact'))->name('contact');
+    Route::get('create', function () {
+        return view('jobs.create');
+    })->name('create');
+
+    Route::post('', function () {
+        // validation...
+
+        Job::create([
+            'title' => request('title'),
+            'salary' => request('salary'),
+            'employer_id' => 1,
+        ]);
+
+        return redirect()->route('jobs.index');
+    })->name('store');
+
+    Route::get('{job}', function (Job $job) {
+        return view('jobs.show', [
+            'job' => $job,
+        ]);
+    })->name('show');
+});
+
+Route::get('/contact', fn() => view('contact'))->name('contact');
