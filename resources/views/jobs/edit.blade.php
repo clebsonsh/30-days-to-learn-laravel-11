2dec861cd79064e5f43e069fa1e04942829b287a
Clebson Moura <clebson.sh@gmail.com>
2024-06-30 18:10:24 -0300

    Routes Reloaded - 6 Essential Tips

--- resources/views/jobs/edit.blade.php
+++ resources/views/jobs/edit.blade.php
@@ -7,41 +7,24 @@
             <div class="border-b border-gray-900/10 pb-12">
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                     <!-- Title -->
-                    <x-input
-                        name="title"
-                        label="Title"
-                        placeholder="Shift Leader"
-                        value="{{ $job->title }}"
-                    />
+                    <x-input name="title" label="Title" placeholder="Shift Leader" value="{{ $job->title }}" />
 
                     <!-- Salary -->
-                    <x-input
-                        name="salary"
-                        label="Salary"
-                        value="{{ $job->salary }}"
-                        placeholder="$50,000 per year"
-                    />
+                    <x-input name="salary" label="Salary" value="{{ $job->salary }}" placeholder="$50,000 per year" />
                 </div>
             </div>
         </div>
 
         <div class="mt-6 flex justify-between">
-            <x-button
-                form="delete-form"
-                color="danger"
-            >
+            <x-button form="delete-form" color="danger">
                 Delete
             </x-button>
 
             <div class="flex items-center gap-x-6">
-                <x-button-link
-                    href="{{ route('jobs.index') }}">
+                <x-button-link href="{{ route('jobs.index') }}">
                     Cancel
                 </x-button-link>
-                <x-button
-                    type="submit"
-                    color="primary"
-                >
+                <x-button type="submit" color="primary">
                     Save
                 </x-button>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -1,65 +1,9 @@
 <?php
 
-use App\Models\Job;
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn() => view('home'))->name('home');
+Route::view('/', 'home')->name('home');
+Route::view('contact', 'contact')->name('contact');
 
-Route::prefix('jobs')->name('jobs.')->group(function () {
-    Route::get('', function () {
-        $jobs = Job::query()
-            ->with('employer')
-            ->latest()
-            ->paginate();
-
-        return view('jobs.index', [
-            'jobs' => $jobs,
-        ]);
-    })->name('index');
-
-    Route::get('create', function () {
-        return view('jobs.create');
-    })->name('create');
-
-    Route::post('', function () {
-        request()->validate(Job::RULES);
-
-        Job::create([
-            'title' => request('title'),
-            'salary' => request('salary'),
-            'employer_id' => 1,
-        ]);
-
-        return redirect()->route('jobs.index');
-    })->name('store');
-
-    Route::get('{job}', function (Job $job) {
-        return view('jobs.show', [
-            'job' => $job,
-        ]);
-    })->name('show');
-
-    Route::get('{job}/edit', function (Job $job) {
-        return view('jobs.edit', [
-            'job' => $job,
-        ]);
-    })->name('edit');
-
-    Route::patch('{job}', function (Job $job) {
-        request()->validate(Job::RULES);
-        $job->update([
-            'title' => request('title'),
-            'salary' => request('salary'),
-        ]);
-
-        return redirect()->route('jobs.show', $job);
-    })->name('update');
-
-    Route::delete('{job}', function (Job $job) {
-        $job->delete();
-
-        return redirect()->route('jobs.index');
-    })->name('destroy');
-});
-
-Route::get('/contact', fn() => view('contact'))->name('contact');
+Route::resource('jobs', JobController::class);
